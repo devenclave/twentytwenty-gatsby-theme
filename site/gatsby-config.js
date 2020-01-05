1d7cdb61ee3d60622510b19feb67c364c24549e7
sagarnasit <sagarnasit@gmail.com>
2020-01-05 18:51:37 +0530

    feat: Add gatsby config files for site and theme

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    siteMetadata: {
+        title: `Twenty Twenty Site`,
+        description: `Gatsby site for Twenty Twenty Gatsby Theme.`,
+        author: `@sagarnasit`,
+        wordPressUrl: `wordpress.com`
+    },
     plugins: [
         {
             resolve: "gatsby-theme-twentytwenty"
